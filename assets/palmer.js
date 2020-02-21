a343d7f8186bc9c13816f06cba4bc92bfd7832e0
anneliesepalmer <anneliesepalmer@gmail.com>
2020-02-21 15:44:31 -0500

    adding weather data to html file for testing

--- assets/palmer.js
+++ assets/palmer.js
@@ -1,20 +1,5 @@
-// API call:
+// API call example:
 // api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
-
-// Parameters:
-// id: City ID
-
-// Examples of API calls:
-// api.openweathermap.org/data/2.5/weather?q=London
-
-// api.openweathermap.org/data/2.5/weather?q=London,uk
-
-// API call:
-// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
-// api.openweathermap.org/data/2.5/weather?zip=94040,us
-
-// Parameters:
-// zip: zip code
  
 // The below code is for the weather api. Below has ORLANDO hard-coded for the project
 //  var city = "USER INPUT HERE"
@@ -32,6 +17,9 @@ console.log(queryURL)
     
     $("#city").text("City: " + response.name);
     $("#humidity").text("Humidity: " + response.main.humidity + "%");
+    $("#sky").text("Sky: " + response.weather[0].description);
+    $("#tempature").text("Tempature: " + response.main.temp);
+
 
 
  });
