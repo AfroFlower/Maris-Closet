94b9400f956ee0fd89ad625efbdca657e16f0070
anneliesepalmer <anneliesepalmer@gmail.com>
2020-02-26 16:20:21 -0500

    updating weahter js file

--- weather-api.js
+++ weather-api.js
@@ -26,12 +26,49 @@ navigator.geolocation.getCurrentPosition(function(p){
         var temp = parseInt(response.main.temp);
         console.log(temp);
 
-        // return as hot if the tempature of the current location is greater than 70
-        if (temp >= 70) {
-            alert("its hot outside!");
-        } else {
-            alert("its cold outside!");
+        if (response.main.temp >= 70) {
+           hot = true;
+        //    alert("its hot outside!");
         }
+        else {
+           hot = false;
+        //    alert("its cold outside!");
+        }
+
+        if (response.main.temp <= 69) {
+            cold = true;
+            // alert("its cold outside!");
+         } 
+         else {
+            cold = false;
+            // alert("its hot outside!");
+         }
+
+        // code if need
+        // // return as hot if the tempature of the current location is greater than 70
+        // if (temp >= 70) {
+        //     hot = true;
+        //     // alert("its hot outside!");
+        // } else {
+        //     hot = false;
+        //     // alert("its cold outside!");
+        // }
+
+    });
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+     if (response.weather[0].main === "Rain"
+     || response.weather[0].main === "Drizzle" 
+     || response.weather[0].main === "Thunderstorm" ) {
+
+        raining = true;
+     } 
+     else {
+        raining = false;
+     }
 
     });
 
@@ -39,4 +76,5 @@ navigator.geolocation.getCurrentPosition(function(p){
     // (e) returns any erros in the code
     console.log(e)
 });
+
 
