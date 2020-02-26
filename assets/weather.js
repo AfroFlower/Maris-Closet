2cd29e6d138e27e4c40df19c69f7cd919b400d4a
AfroFlower <anneliesepalmer@gmail.com>
2020-02-26 09:00:41 -0500

    working on my hot/cold functions

--- assets/weather.js
+++ assets/weather.js
@@ -9,8 +9,8 @@ navigator.geolocation.getCurrentPosition(function(p){
     var lat = geoLat;
     var lon = geoLon;
     
-    // console.log(lat)
-    // console.log(lon)
+    // console.log(lat);
+    // console.log(lon);
 
     var apiKey = "&appid=809719346cfc3a45574c551cf515962c"
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + apiKey + "&units=imperial";
@@ -19,17 +19,29 @@ navigator.geolocation.getCurrentPosition(function(p){
       url: queryURL,
       method: "GET"
     }).then(function(response){
-        console.log(response)
+        console.log(response);
         
         var temp = parseInt(response.main.temp);
-        console.log(temp)
+        console.log(temp);
 
-        hot = parseInt("84" > "60");
+        // var hot = "70" <= temp;
+        // var cold = "65" >= temp;
 
-        if (hot > temp ) {
-            alert("its hot!");
-        } else {
-            alert("its cold!");
+        // if (hot) {
+        //     (alert("its hot outside!"));
+        // } else if (cold) {
+        //     alert("its cold outside!");
+        // }
+
+        var hot = parseInt(70)
+        var cold = parseInt(65)
+
+        if (hot >= temp) {
+            (alert("its a bit hot outside!"));
+        }
+
+        if (cold >= temp) {
+            (alert("its a bit cold outside!"));
         }
 
     });
