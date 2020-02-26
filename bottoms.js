69003e24ce3fdf48c3d39171ca00ba1d8910ac62
meagan bianca <59886559+meaganbianca@users.noreply.github.com>
2020-02-26 12:44:10 -0500

    bottoms
    function works 100%

--- bottoms.js
+++ bottoms.js
@@ -1,6 +1,6 @@
 // user would pick the chosenOccasion
 // casual, business, or formal
-var chosenOccasion = "casual";
+var chosenOccasion = "formal";
 // the weather api function gives us the temp
 var temp = "hot";
 // the weather api function gives us whether or not it is raining
@@ -10,7 +10,7 @@ var rain = "no";
 var finalShoe = {
 image: "picture",
 item: "shoe",
-occasion: "casual",
+occasion: "formal",
 wet: "no",
 color: "yellow",
 neutral: false,  
@@ -21,82 +21,85 @@ var finalTop = {
 image: "picture",
 item: "shirt",
 occasion: "casual",
-length: "hot",
+weatherType: "hot",
 color: "yellow",
 neutral: true,
 };
 
 // each object is a different bottom.
-var a = {
-image: "picture",
-item: "bottom",
-occasion: "casual",
-lengthhh: "hot",
-color: "white",
-neutral: true,
-denim: false, 
-};
-var b = {
-image: "picture",
-item: "bottom",
-occasion: "casual",
-lengthhh: "cold",
-color: "gray",
-neutral: true,
-denim: false, 
-};
-var c = {
-image: "picture",
-item: "bottom",
-occasion: "casual",
-lengthhh: "hot",
-color: "blue",
-neutral: false,
-denim: false,   
-};
-var d = {
-image: "picture",
-item: "bottom",
-occasion: "formal",
-lengthhh: "cold",
-color: "black",
-neutral: true,
-denim: false,  
-};
-var e = {
-image: "picture",
-item: "bottom",
-occasion: "business",
-lengthhh: "hot",
-color: "red",
-neutral: false,
-denim: false,  
-};
+// var a = {
+// image: "a",
+// item: "bottom",
+// occasion: "formal",
+// weatherType: "hot",
+// color: "white",
+// neutral: true,
+// denim: false, 
+// };
+// var b = {
+// image: "picture",
+// item: "bottom",
+// occasion: "casual",
+// weatherType: "cold",
+// color: "gray",
+// neutral: true,
+// denim: false, 
+// };
+// var c = {
+// image: "c",
+// item: "bottom",
+// occasion: "casual",
+// weatherType: "hot",
+// color: "blue",
+// neutral: false,
+// denim: true,   
+// };
+// var d = {
+// image: "d",
+// item: "bottom",
+// occasion: "formal",
+// weatherType: "cold",
+// color: "yellow",
+// neutral: true,
+// denim: false,  
+// };
+// var e = {
+// image: "picture",
+// item: "bottom",
+// occasion: "business",
+// weatherType: "hot",
+// color: "red",
+// neutral: false,
+// denim: false,  
+// };
 var f = {
-image: "picture",
+image: "f",
 item: "bottom",
 occasion: "casual",
-lengthhh: "hot",
+weatherType: "hot",
 color: "blue",
 neutral: false,
 denim: true,  
+wet: "no"   
+
 };
 var g = {
-image: "picture",
+image: "g",
 item: "bottom",
 occasion: "casual",
-lengthhh: "hot",
+weatherType: "hot",
 color: "yellow",
 neutral: false,
-denim: false,   
+denim: false,
+wet: "no"   
 };
 
-var usersBottoms = [a, b, c, d, e, f, g];
-var optionsBottoms = [];
-var finalBottom = {};
+var usersBottoms = [f, g];
 
 // Create a FUNCTION that will narrow bottom options.
-function outfitBottom () {
+function outfitBottom (chosenOccasion, wet, temp, finalTop) {
+    var optionsBottoms = [];
+    var finalBottom = {};
 
     // go through the users bottoms
     for (var i = 0; i < usersBottoms.length; i++) {
@@ -104,111 +107,75 @@ function outfitBottom () {
         // check if the occasion is FORMAL
 
         // if the bottoms match the chosen occasion ...
-        if ((usersBottoms[i].occasion === chosenOccasion)
-        // and if the occasion is FORMAL ...
-        && (usersBottoms[i].occasion === "formal")
-        //  and the bottoms match the final top color ...
-        && ((usersBottoms[i].color === finalTop.color)
-        // or the bottoms are a neutral color ... 
-        || (usersBottoms[i].neutral === true))) {
+        if ( usersBottoms[i].occasion === chosenOccasion
+            // and if the occasion is FORMAL ...
+            && chosenOccasion === "formal"
+            //  and the bottoms match the final top color ...
+            && ( usersBottoms[i].color === finalTop.color
+            // or the bottoms are a neutral color ... 
+            || usersBottoms[i].neutral === true ) ) {
 
             // move matching formal bottoms to the options array
             // (we are not checking weather because all formal bottoms lengthhh should be long)
             optionsBottoms.push(usersBottoms[i]);
-
-            // randomly pick bottoms from options array
-            finalBottom = optionsBottoms[Math.floor(Math.random() * optionsBottoms.length)];
-
-            // console.log(optionsBottoms)
+            // console.log(finalBottom)
 
         } 
 
 
 
-        // check if it is raining
+        // check if it is raining and casual and business
 
         // if the bottoms match the chosen occasion ...
-        else if (((usersBottoms[i].occasion === chosenOccasion)
+        else if ( usersBottoms[i].occasion === chosenOccasion
         // and it is raining ...
-        && (rain = "yes"))
+        && usersBottoms[i].wet === wet
+
+        && wet === "yes"
         // and if the bottom lengthhh is long ...
-        && (usersBottoms[i].lengthhh === "cold")
+        && usersBottoms[i].weatherType === "cold"
         //  and the bottoms match the final top color ...
-        && ((usersBottoms[i].color === finalTop.color)
+        && ( usersBottoms[i].color === finalTop.color
         // or the bottoms are a neutral color ... 
-        || (usersBottoms[i].neutral === true) 
+        || usersBottoms[i].neutral === true 
         // or the bottoms are jeans ...
-        || (usersBottoms[i].denim === true))) {
+        || usersBottoms[i].denim === true ) ) {
 
             // move long bottoms to the options array
-            // (because user should wear long bottoms when it rainss)
+            // (because user should wear long bottoms when it rains)
             optionsBottoms.push(usersBottoms[i]);
-
-            // randomly pick bottoms from options array
-            finalBottom = optionsBottoms[Math.floor(Math.random() * optionsBottoms.length)];
-
-            // console.log(optionsBottoms)            
-
+            // console.log(optionsBottoms)  
         }
 
 
 
-        // check if the weather is cold
+        // check if the weather is cold or hot
 
         // or else if the bottoms matches the chosen occasion...
-        else if (((usersBottoms[i].occasion === chosenOccasion)
+       else if ( usersBottoms[i].occasion === chosenOccasion
         // and the bottoms matches the weather...
-        && (usersBottoms[i].lengthhh === temp))
-        // and if the bottom lengthhh is long ...
-        && (usersBottoms[i].occasion === "cold")  
+        && usersBottoms[i].weatherType === temp
+        // // and if it is not raining  ...
+        && usersBottoms[i].wet === wet
         //  and the bottoms match the final top color ...
-        && ((usersBottoms[i].color === finalTop.color)
+        && ( usersBottoms[i].color === finalTop.color
         // or the bottoms are a neutral color ... 
-        || (usersBottoms[i].neutral === true) 
+        || usersBottoms[i].neutral === true 
         // or the bottoms are jeans ...
-        || (usersBottoms[i].denim === true))) {
+        || usersBottoms[i].denim === true ) ) {
             
             // move matching bottoms to the options array
             optionsBottoms.push(usersBottoms[i]);
-
-            // randomly pick bottom from options array
-            finalBottom = optionsBottoms[Math.floor(Math.random() * optionsBottoms.length)];
-
             // console.log(optionsBottoms) 
-
         }
-
-
-
-        // check if the weather is hot
-
-        // or else if the bottoms matches the chosen occasion...
-        else if (((usersBottoms[i].occasion === chosenOccasion)
-        // and the bottoms matches the weather...
-        && (usersBottoms[i].lengthhh === temp))
-        // and if the bottom lengthhh is short ...
-        && (usersBottoms[i].occasion === "hot")  
-        //  and the bottoms match the final top color ...
-        && ((usersBottoms[i].color === finalTop.color)
-        // or the bottoms are a neutral color ... 
-        || (usersBottoms[i].neutral === true) 
-        // or the bottoms are jeans ...
-        || (usersBottoms[i].denim === true))) {
-            
-            // move matching bottoms to the options array
-            optionsBottoms.push(usersBottoms[i]);
-
-            // randomly pick bottom from options array
-            finalBottom = optionsBottoms[Math.floor(Math.random() * optionsBottoms.length)];
-
-            // console.log(optionsBottoms) 
-
-        }
-
+        
     };
     
-console.log(finalBottom);
+    // randomly pick bottom from options array
+    finalBottom = optionsBottoms[Math.floor(Math.random() * optionsBottoms.length)];
+    console.log(finalBottom);
+    return finalBottom
 
 };
 
-outfitBottom();
+outfitBottom("casual", rain, "hot", finalTop);
