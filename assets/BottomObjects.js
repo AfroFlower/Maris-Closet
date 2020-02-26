4b0f8aad965a7e4dbb2cb0585bf6a760f75e09bf
KweenKrys <calloway.krysn826@gmail.com>
2020-02-26 13:18:34 -0500

    adding objects, and home page

---
+++ assets/BottomObjects.js
@@ -0,0 +1,118 @@
+// Bottoms
+var bottom1 = {
+image: "https://sheinsz.ltwebstatic.com/she_dist/images/bg-grey-ba96515e9b.png",
+item: "bottom",
+occasion: "formal",
+lengthhh: "cold",
+color: "black",
+neutral: true,
+denim: false,
+};
+var bottom2 = {
+image: "https://sheinsz.ltwebstatic.com/she_dist/images/bg-grey-ba96515e9b.png",
+item: "bottom",
+occasion: "business",
+lengthhh: "hot",
+color: "black",
+neutral: true,
+denim: false,
+};
+var bottom3 = {
+image: "https://img.ltwebstatic.com/images2_pi/2018/08/16/15344122864071547532_thumbnail_600x799.webp",
+item: "bottom",
+occasion: "casual",
+lengthhh: "hot",
+color: "blue",
+neutral: true,
+denim: true,
+};
+var bottom4 = {
+image: "https://img.ltwebstatic.com/images2_pi/2019/09/06/15677562642354045488_thumbnail_405x552.webp",
+item: "bottom",
+occasion: "casual",
+lengthhh: "cold",
+color: "white",
+neutral: true,
+denim: false,
+};
+var bottom5 = {
+image: "https://img.ltwebstatic.com/images3_pi/2019/12/20/15768265140a369ddb1f90489f9a2889797a901389_thumbnail_600x.webp",
+item: "bottom",
+occasion: "business",
+lengthhh: "cold",
+color: "beige",
+neutral: true,
+denim: false,
+};
+var bottom6 = {
+image: "https://img.ltwebstatic.com/images3_pi/2019/12/30/157767067867e72dda4e26cfeeb18d4e841e8706f4_thumbnail_600x.webp",
+item: "bottom",
+occasion: "casual",
+lengthhh: "hot",
+color: "black",
+neutral: true,
+denim: false,
+};
+var bottom7 = {
+image: "https://sheinsz.ltwebstatic.com/she_dist/images/bg-grey-ba96515e9b.png",
+item: "bottom",
+occasion: "business",
+lengthhh: "cold",
+color: "black",
+neutral: true,
+denim: false,
+};
+var bottom8 = {
+image: "https://sheinsz.ltwebstatic.com/she_dist/images/bg-grey-ba96515e9b.png",
+item: "bottom",
+occasion: "casual",
+lengthhh: "cold",
+color: "white",
+neutral: true,
+denim: false,
+};
+var bottom9 = {
+image: "https://img.ltwebstatic.com/images3_pi/2019/12/20/15768265140a369ddb1f90489f9a2889797a901389_thumbnail_600x.webp",
+item: "bottom",
+occasion: "business",
+lengthhh: "cold",
+color: "blue",
+neutral: true,
+denim: false,
+};
+var bottom10 = {
+image: "https://sheinsz.ltwebstatic.com/she_dist/images/bg-grey-ba96515e9b.png",
+item: "bottom",
+occasion: "casual",
+lengthhh: "cold",
+color: "purple",
+neutral: true,
+denim: true,
+};
+var bottom11= {
+image: "https://sheinsz.ltwebstatic.com/she_dist/images/bg-grey-ba96515e9b.png",
+item: "bottom",
+occasion: "business",
+lengthhh: "cold",
+color: "white",
+neutral: true,
+denim: false,
+};
+var bottom12 = {
+image: "https://www.fjallraven.com/globalassets/fjallraven/shorts_1/f82462/detail_7323450320621_ss18_a_high_coast_shorts_21.jpg",
+item: "bottom",
+occasion: "business",
+lengthhh: "hot",
+color: "white",
+neutral: true,
+denim: false,
+};
+var bottom13 = {
+image: "https://www.fjallraven.com/globalassets/fjallraven/shorts_1/f82462/detail_7323450098995_ss18_a_high_coast_shorts_21.jpg",
+item: "bottom",
+occasion: "business",
+lengthhh: "hot",
+color: "blue",
+neutral: true,
+denim: false,
+};
---
+++ assets/ShoeObjects.js
@@ -0,0 +1,78 @@
+var shoe1 = {
+image: "https://belk.scene7.com/is/image/Belk?layer=0&src=3900006_25329719_A_719&$DWP_PRODUCT_PDP_LARGE$",
+occasion: "formal",
+wet: "no",
+color: "gold",
+neutral: true,  
+};
+var shoe2 = {
+image: "https://media.aldoshoes.com/v3/product/daywen/600-001-031/daywen_red_600-001-031_main_rc_nt_1000x1270.jpg",
+occasion: "business",
+wet: "no",
+color: "burgundy",
+neutral: true,  
+};
+var shoe3 = {
+image:  "https://c.static-nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2bd27653-9e2a-4cc9-9eab-1c01915c1804/zoom-freak-1-soul-glo-basketball-shoe-tmk8Hw.jpg",
+occasion: "casual",
+wet: "yes",
+color: "yellow",
+neutral: true,  
+};
+var shoe4 = {
+image: "https://c.static-nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c1710532-3afe-4db5-8126-199c3d9f2d79/air-force-1-07-lv8-mens-shoe-1qm6ML.jpg",
+occasion: "casual",
+wet: "yes",
+color: "burgundy",
+neutral: true,  
+};
+var shoe5 = {
+image: "https://media.aldoshoes.com/v3/product/daywen/600-001-031/daywen_red_600-001-031_main_rc_nt_1000x1270.jpg",
+occasion: "business",
+wet: "no",
+color: "red",
+neutral: true,  
+};
+var shoe6 = {
+image: "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMCAwaDMwMHYzMDBIMHoiIGZpbGw9IiNFQ0VERUYiLz48L3N2Zz4=",
+occasion: "casual",
+wet: "yes",
+color: "white",
+neutral: true,  
+};
+var shoe7 = {
+image: "https://media.aldoshoes.com/v3/product/mccrery/001-001-046/mccrery_black_001-001-046_main_rc_nt_350x445.jpg",
+occasion: "business",
+wet: "no",
+color: "black",
+neutral: true,  
+};
+var shoe8 = {
+image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/91f51b14a34841c59fb9a82b0139dc49_9366/terrex-swift-r2-mid-gore-tex-hiking-shoes.jpg",
+occasion: "casual",
+wet: "yes",
+color: "black",
+neutral: true,  
+};
+var shoe9 = {
+image: "https://media.aldoshoes.com/v3/product/daywen/450-001-031/daywen_blue_450-001-031_main_rc_nt_500x635.jpg",
+occasion: "business",
+wet: "no",
+color: "blue",
+neutral: true,  
+};
+var shoe10 = {
+image: "https://c.static-nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f9b8eeb0-2a77-41d3-ad6d-233491fbe365/lebron-17-monstars-basketball-shoe-CR7772.jpg",
+occasion: "casual",
+wet: "yes",
+color: "purple",
+neutral: true,  
+};
+var shoe11 = {
+image: "https://media.aldoshoes.com/v3/product/mirandi/001-003-038/mirandi_black_001-003-038_main_rc_nt_350x445.jpg",
+occasion: "business",
+wet: "no",
+color: "black",
+neutral: true,  
+};
+
